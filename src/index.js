55947c4297e72df8e339436f13c3e3199bd6711a
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-11-29 20:50:54 -0300

    Reset Aplicado ao Projeto

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Reset from "./styles/Reset";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
+        <Reset></Reset>
         <App></App>
     </React.StrictMode>
 );
