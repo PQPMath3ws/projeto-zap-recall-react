35644a593350e54c98b08daff430cdf3e11e3f48
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-12-04 13:02:20 -0300

    Alteração das perguntas do projeto

--- src/const/Cards.js
+++ src/const/Cards.js
@@ -1,12 +1,15 @@
 const Cards = [
-	{ question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
-	{ question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
-	{ question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
-	{ question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
-	{ question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
-	{ question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
-	{ question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
-	{ question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+	{ question: "O que é a etapa de Footprint?", answer: "Etapa realizada para coletar informações sobre um sistema / aplicativo / site / servidor." },
+	{ question: "O que é SQL Injection?", answer: "Técnica de manipulação de input com o objetivo de tentar inserir código SQL a fim de extrair informações úteis dele." },
+	{ question: "O que é o Kali Linux?", answer: "Sistema operacional baseado em Debian (Distro Linux) voltado a segurança digital." },
+	{ question: "Para que serve o WHOIS?", answer: "Coletar possíveis informações sobre o dono de um domínio registrado na internet." },
+	{ question: "Para que serve o DIRB?", answer: "Explorar possíveis diretórios disponíveis (e escondidos) de um domínio / site na internet." },
+	{ question: "O que é DoS?", answer: "Um ataque de negação de serviço." },
+	{ question: "O que é o Google Dorks / Google Hacking?", answer: "Técnica para realizar buscas customizadas no Google para encontrar informações customizadas / úteis sobre um website ou diretório na internet." },
+	{ question: "Para que serve o NMAP?", answer: "Realizar a exploração de serviços rodando nas portas abertas/fechadas de um servidor na internet." },
+	{ question: "Qual a utilidade do OSINT?", answer: "Realizar a busca de informações públicas na internet de sistemas e serviços, sem violar quaisquer leis de privacidade." },
+	{ question: "O que são programas de Bug Bounty?", answer: "São programas disponibilizado por empresas que te pagam por encontrar e reportar, de forma ética, falhas em seus sistemas / sites / apps / jogos." },
+	{ question: "O que é DDoS?", answer: "Um ataque distribuído de negação de serviço." },
 ];
 
 export default Cards;
